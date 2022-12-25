228d8d192542209d20169ab8dd23ab03aa4886ff
said <barchil@gmail.com>
2022-12-25 12:14:53 -0500

    first commit

---
+++ main/java/beta/hello.java
@@ -0,0 +1,13 @@
+package beta;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("***********************");
+		System.out.println("*****  Hello **********");
+		System.out.println("***********************");
+
+	}
+
+}
